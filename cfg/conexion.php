b78836d983a64810daa64811091cf328f525a2ca
Fernando Mejia Romero <fmejia@ucol.mx>
2017-11-27 18:53:05 +0000

    Ya agrega :d #2

--- cfg/conexion.php
+++ cfg/conexion.php
@@ -1,7 +1,18 @@
 <?php
     $servidor = "0.0.0.0";
-    $usuario = "vaictor";
+    $usuario = "fermejia";
+    $password = "";
     $db = "c9";
+    $dbport = 3306;
+    
+        /* Create connection
+    $db = new mysqli($servername, $usuario, $password, $db, $dbport);
+    
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    } 
+    echo "Connected successfully (".$db->host_info.")"; */
 
     $conexion = mysql_connect($servidor, $usuario);
         mysql_set_charset('utf8');
@@ -9,7 +20,7 @@
         die("No pudo conectarse:" . mysql_error());
     }
     else{
-       //echo 'Conectado  satisfactoriamente al servidor <br>';
+       echo 'Conectado  satisfactoriamente al servidor <br>';
     }
     mysql_select_db($db, $conexion) or die(mysql_error($conexion));
 ?>
